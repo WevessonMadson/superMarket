10252522fa3680c8a07cfe74acbbd244360a6858
WevessonMadson <wevesson.madson@hotmail.com>
2023-04-16 19:01:27 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,92 @@
+const tbody = document.querySelector("#tbody");
+const descricao = document.querySelector("#descricao");
+const quantidade = document.querySelector("#quantidade");
+const botaoAdicionar = document.querySelector("#adicionar");
+const totProdSpan = document.querySelector("#totLista");
+const trs = document.getElementsByClassName("trTableValue");
+
+function adicionar(e) {
+    e.preventDefault();
+
+    let prodDescr = descricao.value;
+    let prodQuant = quantidade.value;
+
+    if (prodDescr === '' || prodQuant === '') {
+        alert("É necessário preencher descrição e quantidade.")
+        return;
+    }
+
+    tbody.innerHTML += newLinha(prodDescr, prodQuant);
+    saveData();
+
+    descricao.value = "";
+    quantidade.value = "";
+    descricao.focus();
+}
+
+function newLinha(descricao, quantidade, preco = 0, total= 0) {
+    preco = preco.toFixed(2);
+    total = total.toFixed(2);
+    return `<tr class="trTableValue"><td class="descProd">${descricao}</td>
+    <td><input type="number" onfocus="selectContent()" oninput="atualizaTotais()" class="inputQtd" value="${quantidade}"></td>
+    <td><input type="number" onfocus="selectContent()" oninput="atualizaTotais()"  class="inputPreco" value="${preco}"></td>
+    <td class="total">${total}</td><td><span class="material-symbols-outlined">delete</span></td></tr>`;
+}
+
+function saveData() {
+    const dataMarket = [];
+    let totProd = 0;
+    for (let i = 0; i < trs.length; i++) {
+        let descricao = trs[i].getElementsByClassName("descProd")[0].innerText;
+        let qtd = Number(trs[i].getElementsByClassName("inputQtd")[0].value);
+        let preco = Number(trs[i].getElementsByClassName("inputPreco")[0].value);
+        let total = Number(qtd) * Number(preco);
+        totProd += total;
+        dataMarket.push({ descricao, qtd, preco, total });
+    }
+    localStorage.setItem("dataMarket", JSON.stringify(dataMarket));
+    localStorage.setItem("total", JSON.stringify(totProd));
+}
+
+function getData() {
+    const dataMarket = localStorage.getItem("dataMarket");
+    tbody.innerHTML = "";
+    if (dataMarket) {
+        JSON.parse(dataMarket).forEach(produto => {
+            tbody.innerHTML += newLinha(produto.descricao, produto.qtd, produto.preco, produto.total);
+        });
+        totProdSpan.innerText = Number(localStorage.getItem("total")).toFixed(2).replace(".", ",");
+    }
+}
+
+function atualizaTotais() {
+    saveData();
+    totProdSpan.innerText = Number(localStorage.getItem("total")).toFixed(2).replace(".", ",");
+    const dataMarket = JSON.parse(localStorage.getItem("dataMarket"));
+    for (let i = 0; i < trs.length; i++) {
+        trs[i].getElementsByClassName("total")[0].innerText = dataMarket[i].total.toFixed(2);
+    }
+}
+
+function selectContent() {
+    let curElement = document.activeElement;
+    curElement.select();
+}
+
+function deleteProd(e) {
+    let elExcluir = e.target;
+
+    if (elExcluir.innerText == 'delete') {
+        let elLinha = elExcluir.parentNode.parentNode;
+        let descricao = elLinha.childNodes[0].innerText;
+        if (confirm(`Confirma a exclusão do produto: "${descricao}" ?`)) {
+            elLinha.remove();
+            saveData();
+            getData();
+        }
+    }
+}
+
+window.addEventListener("DOMContentLoaded", getData);
+botaoAdicionar.addEventListener("click", adicionar);
+tbody.addEventListener("click", deleteProd);
