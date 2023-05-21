aba3daaa5b1a830563fe3d2abf8746ce7db04cc0
WevessonMadson <wevesson.madson@hotmail.com>
2023-05-21 08:34:33 -0300

    testar pwa

---
+++ sw.js
@@ -0,0 +1,33 @@
+// 1- o nome do cache
+
+const cacheName = 'v1';
+
+// 2- os resources que serão salvos no cache;
+
+const resourcesToPrecache = [
+    "/",
+    "index.html",
+    "/assets/favicon.png",
+    "/assets/github-icon.png",
+    "/assets/linkedin-icon.png",
+    "/assets/android/*",
+    "/assets/windows11/*",
+    "/assets/ios/*",
+    "/css/index.css",
+    "/js/index.js",
+    "manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUtil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(resourcesToPrecache)),
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cacheResponse => (cacheResponse || fetch(event.request))),
+    );
+});
