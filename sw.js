fbbaf6ebae75ef6c9befce9df9d84f37e5ac8634
WevessonMadson <wevesson.madson@hotmail.com>
2023-05-22 21:18:31 -0300

    file sw

--- sw.js
+++ sw.js
@@ -56,6 +56,6 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(cacheResponse => (cacheResponse || fetch(event.request))),
+            .then(cacheResponse => (cacheResponse)),
     );
 });
